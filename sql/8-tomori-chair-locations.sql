f09a0e96d9dc9dc1a74ad7f06ba76cc8c5bea448
Takuma Tomori <tmrtkm1227tksx@gmail.com>
2024-12-08 16:50:19 +0900

    [feat] ちょっと修正

--- sql/8-tomori-chair-locations.sql
+++ sql/8-tomori-chair-locations.sql
@@ -1,13 +1,2 @@
--- 走行距離管理用のテーブルを作成
-DROP TABLE IF EXISTS chair_mileage;
-CREATE TABLE chair_mileage
-(
-  chair_id   VARCHAR(26) NULL COMMENT '割り当てられた椅子ID',
-  distance   INTEGER NOT NULL COMMENT '総走行距離',
-  updated_at DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) COMMENT '最終走行距離更新日時',
-  UNIQUE (chair_id)
-)
-  COMMENT '椅子ごとの総走行距離テーブル';
-
 -- chair_locationsのchair_idをユニークにする
 ALTER TABLE chair_locations ADD CONSTRAINT unique_chair_id UNIQUE (chair_id);
